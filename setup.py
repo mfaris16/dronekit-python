aa4a464bb8b8fe9e8d164da85cc52eb30b68186e
Muhammad Faris Adi Prabowo <muhammadfarisadiprabowo1@gmail.com>
2020-04-08 19:19:55 +0700

    Update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     description='Developer Tools for Drones.',
     long_description_content_type="text/markdown",
     long_description=LongDescription,
-    url='https://github.com/dronekit/dronekit-python',
+    url='https://github.com/mfaris16/dronekit-python',
     author='3D Robotics',
     install_requires=[
         'pymavlink>=2.2.20',
